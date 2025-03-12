57eb66209c1700a8d4a899822174924a995d1c7b
LowLightCZ1 <155197914+LowLightCZ1@users.noreply.github.com>
2025-03-12 12:47:16 +0100

    Burger menu web

---
+++ School/Web_Text/JavaScrypt/scrypt.js
@@ -0,0 +1,7 @@
+const color = ["black", "gray"]
+const header = [...document.getElementsByTagName("header")]
+
+window.onscroll = () => {
+    const scrollFromTop = window.pageXOffset
+    for (let i= 0, header.length,  )
+}
