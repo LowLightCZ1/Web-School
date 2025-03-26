77cac74420d2ed02dcd0b8089fee7ca669a7c724
LowLightCZ1 <155197914+LowLightCZ1@users.noreply.github.com>
2025-03-26 11:06:41 +0100

    Roztřídění

--- School/Web_Text/JavaScrypt/scrypt.js
+++ School/Web_Text/JavaScrypt/scrypt.js
@@ -1,7 +1,19 @@
-const color = ["black", "gray"]
-const header = [...document.getElementsByTagName("header")]
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.getElementById('1');
+    let lastScrollY = window.scrollY;
 
-window.onscroll = () => {
-    const scrollFromTop = window.pageXOffset
-    for (let i= 0, header.length,  )
-}
+    window.addEventListener('scroll', () => {
+        const currentScrollY = window.scrollY;
+        
+        // Check scroll direction
+        if (currentScrollY > lastScrollY) {
+            // Scrolling down 
+            header.style.transform = `translateY(${window.scrollY /1}px)`;
+        } else {
+            // Scrolling up - show header
+            header.style.transform = `translateY(${window.scrollY /1}px)`;
+        }
+        
+        lastScrollY = currentScrollY;
+    });
+});
---
+++ Web_Canvas/JavaScript/script.js
@@ -0,0 +1,14 @@
+// Získání referenci na canvas
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+// malování šachvnice
+for(let y  = 0; y <8; y++) {
+    for(let x  = 0; x <8; x++) {
+        ctx.beginPath();
+        ctx.rect(x*100, y * 100, 100, 100); // obdélník
+        ctx.fillStyle = (x + y) % 2 ? "#fff": "#000";
+        ctx.fill();
+    }
+    
+}
