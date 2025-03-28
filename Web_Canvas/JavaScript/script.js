a1644be2a49ccf5f118e55e0dafd0f62c38d60b1
LowLightCZ1 <155197914+LowLightCZ1@users.noreply.github.com>
2025-03-28 12:40:00 +0100

    Chess game update

--- Web_Canvas/JavaScript/script.js
+++ Web_Canvas/JavaScript/script.js
@@ -1,9 +1,11 @@
 // Získání referenci na canvas
-const canvas = document.getElementById("canvas");
+const canvas = document.getElementById("board");
 const ctx = canvas.getContext("2d");
 
+const bichop = document.getElementById("1")
+
 // malování šachvnice
-for(let y  = 0; y <8; y++) {
+for(let y = 0; y <8; y++) {
     for(let x  = 0; x <8; x++) {
         ctx.beginPath();
         ctx.rect(x*100, y * 100, 100, 100); // obdélník
