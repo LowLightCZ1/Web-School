e66869948ce49fdbbacf6874c6508838bf5d79c3
Matej Hajny <hajny.mate8@gmail.com>
2026-03-05 19:57:21 +0100

    slot machine done

--- web-maty/assets/js/script.js
+++ web-maty/assets/js/script.js
@@ -1,3 +1,17 @@
-const slotCont = document.getElementById("slotCont");
-slotCont.querySelector("slot");
+const slots = document.getElementsByClassName("slot");
 
+console.log(slots.item(1));
+
+function slotBtnFunc(button){
+    const clicked = button.classList.contains("clicked");
+
+    if(!clicked){
+        button.classList.add("clicked");
+        button.textContent = "STOP";
+        
+    }
+    else{
+        button.classList.remove("clicked");
+        button.textContent = "HRA";
+    }
+};
