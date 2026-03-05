5217c996f5fad40c92b17557f33155e1da1ac49e
Matej Hajny <hajny.mate8@gmail.com>
2026-03-05 19:57:38 +0100

    done

--- web-maty/assets/js/script.js
+++ web-maty/assets/js/script.js
@@ -1,6 +1,7 @@
-const slots = document.getElementsByClassName("slot");
+const slots = document.querySelectorAll("h2");
+let interval;
 
-console.log(slots.item(1));
+let slotValues = [0,0,0]
 
 function slotBtnFunc(button){
     const clicked = button.classList.contains("clicked");
@@ -8,10 +9,27 @@ function slotBtnFunc(button){
     if(!clicked){
         button.classList.add("clicked");
         button.textContent = "STOP";
-        
+        interval = setInterval(() => {slots.forEach(s => s.innerHTML = Math.floor(Math.random() * 10))}, 100)
     }
-    else{
+    else{ 
         button.classList.remove("clicked");
         button.textContent = "HRA";
+        slots.innerHTML = "";
+        clearInterval(interval);
+        
+        slotValues = Array.from(slots).map(s => Number(s.textContent));
+        checkWin(slotValues);
     }
-};
+
+    function checkWin(values){
+        const win = values.every(val => val===values[0]);
+
+        if(win){
+            alert("JACKPOT! Všechna čísla jsou: " + values[0]);
+        }
+        else{
+            
+        }
+    }
+
+}
